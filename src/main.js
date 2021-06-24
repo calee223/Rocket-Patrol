b1e9dd87894b7ef5c1625c91ef5cb4fb7d7b2670
Christopher <clee223@ucsc.edu>
2021-06-24 19:24:43 -0400

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,24 @@
+//console.log("Rocket Patrol");
+
+let config = {
+    type: Phaser.CANVAS,
+    width: 800,
+    height: 600,
+    scene: [Menu, Play]
+}
+
+
+
+let game = new Phaser.Game(config);
+
+
+let borderUISize = game.config.height /15;
+let borderPadding = borderUISize / 3;
+
+
+//reserve keyboard vars
+
+let keyF, keyR, keyLEFT, keyRIGHT;
+
+
+
